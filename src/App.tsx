544fadc00697dc223cb310cb0f2224686ff91c8a
swanine <haohao1500@foxmail.com>
2022-04-17 16:23:57 +0800

    🔧 chore: 创建项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { RouterView } from 'vue-router'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  render() {
+    return (
+      <>
+        <RouterView></RouterView>
+      </>
+    )
+  }
+})
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src')
+    }
+  },
+  base: './',
+  server: {
+    port: 8090,
+    open: true
+  }
+})
