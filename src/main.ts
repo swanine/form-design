43d0702c6fe58a46f7657226c2e9b04950e09492
swanine <haohao1500@foxmail.com>
2022-04-20 15:54:40 +0800

    💫 refactor: 调整样式文件资源

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App'
-import 'element-plus/dist/index.css'
 import router from './router/index'
 import store from './store/index'
+import 'loaders.css/loaders.min.css'
 import './assets/styles/index.scss'
 import 'intro.js/introjs.css'
+import 'element-plus/dist/index.css'
+import 'hint.css/hint.min.css'
 
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
